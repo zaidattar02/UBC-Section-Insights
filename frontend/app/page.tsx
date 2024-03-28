39d84cd6f23f1838e7b5a74dcc720faf21e89a44
UBC Student <ezheng09@students.cs.ubc.ca>
2024-03-27 17:26:23 -0700

    Initialize the Frontend (#35)
    * Create Next App
    
    * update lockfiles

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "InsightUBC",
+  description: "Sections Insights for UBC Students",
+}
+
+export default function Home() {
+  return (
+    <main></main>
+  );
+}
