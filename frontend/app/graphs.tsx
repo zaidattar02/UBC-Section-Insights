b11f295c8e485e05c3d5dea57a78384dcf98b9bc
zaid <zalattar@students.ubc.ca>
2024-04-03 22:02:55 -0700

    setting up recharts

--- frontend/app/graphs.tsx
+++ frontend/app/graphs.tsx
@@ -1,4 +1,8 @@
+"use client"
 import { DatasetInterface } from "~/types/Dataset";
+import { useEffect, useState } from "react";
+import { apiURL } from "./const";
+import { BarChart, Bar, XAxis, YAxis } from 'recharts';
 
 export default function Graphs({selectedDataset}: {selectedDataset: DatasetInterface}) {
     return (
